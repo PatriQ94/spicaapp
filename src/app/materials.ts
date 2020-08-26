705f503dbfd654c5e52a000bfd93e8f34f08ba47
Pa7r1Q ForeveR <patro11@hotmail.com>
2020-08-26 11:36:10 +0200

    Improved settings page and added help tooltip

--- src/app/materials.ts
+++ src/app/materials.ts
@@ -5,12 +5,13 @@ import { MatDividerModule} from '@angular/material/divider';
 import {MatCardModule} from '@angular/material/card';
 import {MatInputModule} from '@angular/material/input';
 import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatTooltipModule} from '@angular/material/tooltip';
 import { NgModule } from '@angular/core';
 import { FlexLayoutModule } from '@angular/flex-layout'
 
 @NgModule ({
-  imports: [MatButtonModule,MatToolbarModule, MatIconModule, MatDividerModule, MatCardModule, MatInputModule, MatFormFieldModule, FlexLayoutModule],
-  exports: [MatButtonModule, MatToolbarModule, MatIconModule, MatDividerModule, MatCardModule, MatInputModule, MatFormFieldModule, FlexLayoutModule],
+  imports: [MatButtonModule,MatToolbarModule, MatIconModule, MatDividerModule, MatCardModule, MatInputModule, MatFormFieldModule, FlexLayoutModule, MatTooltipModule],
+  exports: [MatButtonModule, MatToolbarModule, MatIconModule, MatDividerModule, MatCardModule, MatInputModule, MatFormFieldModule, FlexLayoutModule, MatTooltipModule],
 })
 
 export class MaterialModules{}
